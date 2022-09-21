d62bcc30c130e349f3c24996f2011d2fa8107078
Weverson Silva <71471668+weverson97@users.noreply.github.com>
2022-09-21 20:29:27 -0300

    Add files via upload

---
+++ Cap4_Função.py
@@ -0,0 +1,24 @@
+def somar(a, b):
+    return a + b
+
+def subtrair(a, b):
+    return a - b
+
+def dividir(a, b):
+    if b != 0:
+        return a / b
+
+def multiplicar(a, b):
+    return a * b
+
+resul = somar(1, 2)
+print(resul)
+
+resul = subtrair(1, 2)
+print(resul)
+
+resul = multiplicar(3, 2)
+print(resul)
+
+resul = dividir(3, 2)
+print(resul)
