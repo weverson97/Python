a35558aa9081e12ef64fbea1a4db522b6ad33a71
Weverson Silva <71471668+weverson97@users.noreply.github.com>
2022-09-20 23:04:40 -0300

    Add files via upload

---
+++ Cap4_Comandos.py
@@ -0,0 +1,16 @@
+import Cap4_Calculadora
+
+resul = Cap4_Calculadora.somar(1, 2)
+print(resul)
+
+resul = Cap4_Calculadora.subtrair(1, 2)
+print(resul)
+
+resul = Cap4_Calculadora.multiplicar(3, 2)
+print(resul)
+
+resul = Cap4_Calculadora.dividir(3, 2)
+print(resul)
+
+
+
---
+++ Cap4_Ex1_Array.py
@@ -0,0 +1,34 @@
+lista = ["Weverson", "Wallace", "Marilene", "Jessica"]
+
+# Exibe lista atual
+print(lista)
+
+# Remove o ultimo da lista
+lista.pop()
+
+# Exibe lista atual
+print(lista)
+
+# Remove a posição desejada, nesse caso, a posição 0
+lista.pop(0)
+
+# Exibe lista atual
+print(lista)
+
+# Remove o nome na lista se encontrar 
+lista.remove("Marilene")
+
+# Exibe lista atual
+print(lista)
+
+# Adiciona no final da lista
+lista.append("Weverson")
+
+# Exibe lista atual
+print(lista)
+
+# Adiciona na posição desejada da lista, nesse caso, na primeira posição
+lista.insert(0, "Jessica")
+
+# Exibe lista atual
+print(lista)
